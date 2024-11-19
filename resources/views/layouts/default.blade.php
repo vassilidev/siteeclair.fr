e1dbd3e80ffca8c81dfbadb08c1b0c15b1c6419a
vassilidev <vassilidevnet@gmail.com>
2024-11-19 11:28:06 +0100

    feat: render fix

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,47 +4,36 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {!! SEO::generate() !!}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-    <!-- Polices -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"
+          media="print" onload="this.media='all'">
 
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/8d4e15b026.js" crossorigin="anonymous"></script>
+    @vite('resources/css/app.css')
 
-    <script src="{{ asset('particles.min.js') }}" defer></script>
-
-    <!-- Styles -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- Favicon et Icônes Apple -->
     <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}" sizes="96x96"/>
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}"/>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}"/>
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}"/>
     <meta name="apple-mobile-web-app-title" content="Site éclair"/>
     <link rel="manifest" href="{{ asset('site.webmanifest') }}"/>
+
+    {!! SEO::generate() !!}
 </head>
 <body class="font-poppins text-text bg-background overflow-x-hidden">
 
-<!-- Curseur personnalisé -->
 <div id="cursor"></div>
 
-<!-- Navbar -->
 @include('layouts.default.navbar')
 
-<!-- Contenu principal -->
 <main class="pt-16">
     @yield('content')
 </main>
 
-<!-- Footer -->
 @include('layouts.default.footer')
 
-<!-- Scripts -->
 @production
-    <!-- Script Tawk.to Live Chat -->
     <script type="text/javascript">
         var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
         (function () {
@@ -57,5 +46,9 @@
         })();
     </script>
 @endproduction
+
+<script defer src="https://kit.fontawesome.com/8d4e15b026.js" crossorigin="anonymous"></script>
+<script defer src="{{ asset('particles.min.js') }}"></script>
+@vite('resources/js/app.js')
 </body>
 </html>
