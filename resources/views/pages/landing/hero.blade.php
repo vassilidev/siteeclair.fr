a27a263bcec9af85d579c85330c2953b0eaa381c
vassilidev <vassilidevnet@gmail.com>
2024-11-19 14:42:10 +0100

    fix: speed

--- resources/views/pages/landing/hero.blade.php
+++ resources/views/pages/landing/hero.blade.php
@@ -1,15 +1,16 @@
-<section class="banner-section relative bg-cover bg-center min-h-screen-with-navbar flex items-center justify-center text-white">
-<div class="absolute inset-0 bg-black opacity-80"></div>
-<div class="relative z-10 text-center px-4">
-    <h1 class="text-3xl md:text-4xl font-bold mb-6">
-        ⚡ Votre site web sur-mesure, performant et rapide 🚀
-    </h1>
-    <p class="text-xl md:text-1xl mb-8">
-        ⏱️ Livré en 5 jours, avec nom de domaine, hébergement, et ajustements illimités inclus.
-    </p>
-    <a href="#about"
-       class="inline-block bg-accent hover:bg-primary text-white font-bold py-3 px-8 rounded-full transition duration-300">
-        Créez votre site maintenant
-    </a>
-</div>
+<section class="relative bg-cover bg-center min-h-screen-with-navbar flex items-center justify-center text-white"
+         style="background-color: #000; background-image: url('{{ asset('img/banner.webp') }}');">
+    <div class="absolute inset-0 bg-black opacity-80"></div>
+    <div class="relative z-10 text-center px-4">
+        <h1 class="text-3xl md:text-4xl font-bold mb-6">
+            ⚡ Votre site web sur-mesure, performant et rapide 🚀
+        </h1>
+        <p class="text-xl md:text-1xl mb-8">
+            ⏱️ Livré en 5 jours, avec nom de domaine, hébergement, et ajustements illimités inclus.
+        </p>
+        <a href="#about"
+           class="inline-block bg-accent hover:bg-primary text-white font-bold py-3 px-8 rounded-full transition duration-300">
+            Créez votre site maintenant
+        </a>
+    </div>
 </section>
